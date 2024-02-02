e485ec9901b725686d726ce4efe722da49cb31d7
Dima4uk <dima.4uk11@gmail.com>
2024-02-02 20:07:03 +0200

    Add class Human

---
+++ CSarpExamTask/Entities/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSarpExamTask.Entities;
+
+public class Human
+{
+    private static int idCounter = 1;
+
+    public int Id { get; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Human()
+    {
+        Id = idCounter++;
+    }
+
+}
--- CSarpExamTask/Program.cs
+++ CSarpExamTask/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSarpExamTask.Entities;
+
 Console.WriteLine("Hello, World!");
+
+Human human1 = new Human() { Name = "Test", Surname = "Tester" };
+Human human2 = new Human() { Name = "Test", Surname = "Tester" };
+Human human3 = new Human() { Name = "Test", Surname = "Tester" };
+
+Console.WriteLine(human1.Id);
+Console.WriteLine(human2.Id);
+Console.WriteLine(human3.Id);
