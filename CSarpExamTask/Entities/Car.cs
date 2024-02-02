e336ad9678daaeb791e0d0a3bc7c08d8d10d0308
Dima4uk <dima.4uk11@gmail.com>
2024-02-02 21:10:13 +0200

    Add class Car

---
+++ CSarpExamTask/Entities/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSarpExamTask.Entities;
+public enum Models
+{
+    BMW,
+    Mercedes,
+    Mitsubishi,
+    Mazda,
+    Audi,
+    Volkswagen,
+    Tesla,
+    Skoda,
+    Dodge,
+    Jeep
+}
+public class Car
+{
+    private static int idCounter = 1;
+
+    public int Id { get; }
+    public int HumanId { get; set; }
+    public Models Model { get; set; }
+    public double EngineSize {  get; set; }
+
+
+   public Car()
+    {
+        Id = idCounter++;
+    }
+}
--- CSarpExamTask/Program.cs
+++ CSarpExamTask/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CSarpExamTask.Entities;
+﻿using CSarpExamTask.Entities;
 
 Console.WriteLine("Hello, World!");
 
@@ -10,3 +9,11 @@ Human human3 = new Human() { Name = "Test", Surname = "Tester" };
 Console.WriteLine(human1.Id);
 Console.WriteLine(human2.Id);
 Console.WriteLine(human3.Id);
+
+Car car1 = new Car() { HumanId = human3.Id, Model = Models.Skoda, EngineSize = 2.5 };
+Car car2 = new Car() { HumanId = human2.Id, Model = Models.Mazda, EngineSize = 2.3 };
+Car car3 = new Car() { HumanId = human1.Id, Model = Models.Tesla, EngineSize = 335 };
+
+Console.WriteLine(car1.Id);
+Console.WriteLine(car2.Id);
+Console.WriteLine(car3.Id);
